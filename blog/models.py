e7c758dd961aaba165d4a5910853fb49e9c3c4bc
dante <thornhillfox@gmail.com>
2020-11-07 14:51:21 +0300

    taggit add

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-# from taggit.managers import TaggableManager
+from taggit.managers import TaggableManager
 # Create your models here.
 
 
@@ -27,7 +27,7 @@ class Post(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
 	status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
-	# tags = TaggableManager()
+	tags = TaggableManager()
 
 	def get_absolute_url(self):
 		return reverse('blog:post_detail', 
